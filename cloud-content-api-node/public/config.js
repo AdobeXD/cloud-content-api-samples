fb0a8629a9db8810eec8131c31707ba072aa5914
Ash <ash@ashryanbeats.com>
2019-11-22 10:16:32 -0500

    Fixing requires for config module. Code formatting updates.

--- cloud-content-api-node/public/config.js
+++ cloud-content-api-node/public/config.js
@@ -2,14 +2,13 @@
 and "YOUR_ADOBE_API_SECRET" with your API Secret */
 
 const apiKey = "YOUR-API-KEY";
-const accessToken = "USER-ACCESS-TOKEN"
+const accessToken = "USER-ACCESS-TOKEN";
 
 try {
-        if (module) {
-                module.exports = {
-                        apiKey,
-                        accessToken
-                }
-        }
-}
-catch (err) { }
+  if (module) {
+    module.exports = {
+      apiKey,
+      accessToken
+    };
+  }
+} catch (err) {}
